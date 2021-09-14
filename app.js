ab7a79276967bc6fb2d8dab848fe866c4f796e1e
Angus Kellett <anguskellett@hotmail.com>
2021-09-14 04:54:11 +0000

    imports url components from .env file

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const url = 'mongodb://localhost/AlienDBex';
+const MONGO_HOSTNAME = process.env.MONGO_HOSTNAME;
+const MONGO_PORT = process.env.MONGO_PORT;
+const MONGO_DB = process.env.MONGO_DB;
+const DB_URL = `mongodb://${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}`;
+const PORT = process.env.PORT || 3000;
 const app = express();
 
-mongoose.connect(url, {useNewUrlParser: true});
+mongoose.connect(DB_URL, {useNewUrlParser: true});
 
 const con = mongoose.connection;
 
 con.on('open', () => {
   console.log("connected...")
 });
+
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}...`);
+})
